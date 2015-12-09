03b1f6e1dfbeaa259576431b4b57bb9bd9902f06
Andrew McRae <dev@fesh.geek.nz>
2015-12-09 18:19:30 +1300

    add some error handling
    give an error if an unexpected character appears in spec string

--- day1/day1.go
+++ day1/day1.go
@@ -2,13 +2,16 @@ package main
 
 import "fmt"
 
-func getFloor(s string) (floor int) {
+func getFloor(s string) (floor int, err error) {
 	for _, c := range s {
 		switch c {
 		case '(':
 			floor++
 		case ')':
 			floor--
+		default:
+			err = fmt.Errorf("Character '%c' invalid", c)
+			return
 		}
 	}
 	return
@@ -16,6 +19,10 @@ func getFloor(s string) (floor int) {
 
 func main() {
 	s := "())"
-	f := getFloor(s)
-	fmt.Printf("spec: '%s'\nfloor: %d\n", s, f)
+	f, err := getFloor(s)
+	if err != nil {
+		fmt.Print(err)
+	} else {
+		fmt.Printf("spec: '%s'\nfloor: %d\n", s, f)
+	}
 }
