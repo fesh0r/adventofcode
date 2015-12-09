f16d712fe5e214ceb53ea08631753f1016de9822
Andrew McRae <dev@fesh.geek.nz>
2015-12-10 11:11:03 +1300

    Add basic tests using provided examples

---
+++ day1/day1_test.go
@@ -0,0 +1,30 @@
+package main
+
+import "testing"
+
+func TestGetFloor(t *testing.T) {
+	floortests := []struct {
+		in  string
+		out int
+	}{
+		{"", 0},
+		{"(())", 0},
+		{"()()", 0},
+		{"(((", 3},
+		{"(()(()(", 3},
+		{"))(((((", 3},
+		{"())", -1},
+		{"))(", -1},
+		{")))", -3},
+		{")())())", -3},
+	}
+
+	for _, tt := range floortests {
+		f, err := getFloor(tt.in)
+		if err != nil {
+			t.Errorf("getFloor(%q) expected: %d, got error: %s", tt.in, tt.out, err)
+		} else if f != tt.out {
+			t.Errorf("getFloor(%q) expected: %d, got: %d", tt.in, tt.out, f)
+		}
+	}
+}
