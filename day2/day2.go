044fe62c8503ba06f61db54cd6e2c8fae732dfa7
Andrew McRae <dev@fesh.geek.nz>
2015-12-11 15:36:32 +1300

    day2 work in progress
    regex for parsing dimension string
    area calculation
    all current tests now passing, need to add file reading and sum of areas

--- day2/day2.go
+++ day2/day2.go
@@ -1,12 +1,49 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"sort"
+	"strconv"
+)
 
 func getWrapping(s string) (area int, err error) {
+	r, err := regexp.Compile("^(\\d+)x(\\d+)x(\\d+)$")
+	if err != nil {
+		return
+	}
+
+	m := r.FindStringSubmatch(s)
+	if m == nil {
+		err = fmt.Errorf("invalid size string %q", s)
+		return
+	}
+
+	l := make([]int, 3)
+	for i := 0; i < 3; i++ {
+		l[i], err = strconv.Atoi(m[i+1])
+		if err != nil {
+			return
+		}
+	}
+
+	sort.Ints(l)
+
+	area = 2*l[0]*l[1] + 2*l[0]*l[2] + 2*l[1]*l[2] + l[0]*l[1]
+
 	return
 }
 
 func run() int {
+	s := "2x3x4"
+	w, err := getWrapping(s)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+
+	fmt.Printf("%q => %d\n", s, w)
 	return 0
 }
 
