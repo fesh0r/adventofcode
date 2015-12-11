9f26ff7724e126fc1670e73404480984a6918a78
Andrew McRae <dev@fesh.geek.nz>
2015-12-11 17:14:46 +1300

    day3 wip
    parse direction string and count houses visited
    all tests passing

--- day3/day3.go
+++ day3/day3.go
@@ -1,12 +1,51 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 func getHouses(s string) (houses int, err error) {
+	var pos [2]int
+
+	h := make(map[[2]int]bool)
+
+	h[pos] = true
+
+	for _, c := range s {
+		switch c {
+		case '<':
+			pos[0]--
+		case '>':
+			pos[0]++
+		case '^':
+			pos[1]++
+		case 'v':
+			pos[1]--
+		default:
+			err = fmt.Errorf("invalid character %q", c)
+			return
+		}
+		h[pos] = true
+	}
+
+	for k := range h {
+		if h[k] {
+			houses++
+		}
+	}
 	return
 }
 
 func run() int {
+	s := ">"
+	h, err := getHouses(s)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+
+	fmt.Printf("houses: %d\n", h)
 	return 0
 }
 
