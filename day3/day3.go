33202af5296868f72249d3d8ea99cefca0e11a5b
Andrew McRae <dev@fesh.geek.nz>
2015-12-14 10:39:56 +1300

    day3 part 2 complete
    add code, tests and example for day3 part 2
    all test pass and answer correct

--- day3/day3.go
+++ day3/day3.go
@@ -39,6 +39,40 @@ func getHouses(s string) (houses int, err error) {
 	return
 }
 
+func getHousesDouble(s string) (houses int, err error) {
+	var pos [2][2]int
+
+	h := make(map[[2]int]bool)
+
+	h[pos[0]] = true
+	h[pos[1]] = true
+
+	for i, c := range s {
+		worker := i % 2
+		switch c {
+		case '<':
+			pos[worker][0]--
+		case '>':
+			pos[worker][0]++
+		case '^':
+			pos[worker][1]++
+		case 'v':
+			pos[worker][1]--
+		default:
+			err = fmt.Errorf("invalid character %q", c)
+			return
+		}
+		h[pos[worker]] = true
+	}
+
+	for k := range h {
+		if h[k] {
+			houses++
+		}
+	}
+	return
+}
+
 func run() int {
 	if len(os.Args) != 2 {
 		fmt.Fprintf(os.Stderr, "%s filename\n", os.Args[0])
@@ -58,7 +92,14 @@ func run() int {
 		return 1
 	}
 
+	h2, err := getHousesDouble(s)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+
 	fmt.Printf("houses: %d\n", h)
+	fmt.Printf("houses2: %d\n", h2)
 	return 0
 }
 
--- day3/day3_test.go
+++ day3/day3_test.go
@@ -39,9 +39,50 @@ func TestGetHousesError(t *testing.T) {
 	}
 }
 
+func TestGetHousesDouble(t *testing.T) {
+	tests := []struct {
+		in  string
+		out int
+	}{
+		{"", 1},
+		{"^v", 3},
+		{"^>v<", 3},
+		{"^v^v^v^v^v", 11},
+	}
+
+	for _, tt := range tests {
+		h, err := getHousesDouble(tt.in)
+		if err != nil {
+			t.Errorf("getHousesDouble(%q) expected: %d, got error: %s", tt.in, tt.out, err)
+		} else if h != tt.out {
+			t.Errorf("getHousesDouble(%q) expected: %d, got: %d", tt.in, tt.out, h)
+		}
+	}
+}
+
+func TestGetHousesDoubleError(t *testing.T) {
+	tests := []string{
+		" ",
+		"A",
+	}
+	for _, tt := range tests {
+		h, err := getHousesDouble(tt)
+		if err == nil {
+			t.Errorf("getHousesDouble(%q) expected error, got: %d", tt, h)
+		}
+	}
+}
+
 func ExampleGetHouses() {
 	s := "^>v<"
 	h, _ := getHouses(s)
 	fmt.Printf("%q => %d\n", s, h)
 	// Output: "^>v<" => 4
+}
+
+func ExampleGetHousesDouble() {
+	s := "^>v<"
+	h, _ := getHousesDouble(s)
+	fmt.Printf("%q => %d\n", s, h)
+	// Output: "^>v<" => 3
 }
