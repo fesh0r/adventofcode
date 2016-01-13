20b2fd602c79604e89c6ee37bfe2a7307b37471d
Andrew McRae <dev@fesh.geek.nz>
2016-01-13 16:20:40 +1300

    day11 part 1 complete
    all tests pass and solution correct

---
+++ day11/day11.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func incRune(r rune) (rune, bool, error) {
+	if r < 'a' || r > 'z' {
+		err := fmt.Errorf("invalid character %q", r)
+		return 0, false, err
+	}
+
+	carry := false
+	r++
+	if r > 'z' {
+		r = 'a'
+		carry = true
+	}
+
+	return r, carry, nil
+}
+
+func incString(r []rune) ([]rune, error) {
+	for i := len(r) - 1; i >= 0; i-- {
+		v, carry, err := incRune(r[i])
+		if err != nil {
+			return []rune{}, err
+		}
+
+		r[i] = v
+
+		if !carry {
+			break
+		} else if i == 0 {
+			r = append([]rune{v}, r...)
+		}
+	}
+
+	return r, nil
+}
+
+func hasNoBad(r []rune) bool {
+	for _, c := range r {
+		switch c {
+		case 'i', 'o', 'l':
+			return false
+		}
+	}
+
+	return true
+}
+
+func hasStraight(r []rune) bool {
+	for i := 0; i < len(r)-2; i++ {
+		if (r[i+1] == (r[i] + 1)) && (r[i+2] == (r[i] + 2)) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func hasRepeated(r []rune) bool {
+	seen := make(map[rune]bool)
+
+	count := 0
+	for i := 0; i < len(r)-1; i++ {
+		if r[i+1] == r[i] && !seen[r[i]] {
+			count++
+			seen[r[i]] = true
+		}
+	}
+
+	return count > 1
+}
+
+func nextPassword(s string) (string, error) {
+	r := []rune(s)
+
+	if len(r) > 8 {
+		err := fmt.Errorf("invalid initial password %q", s)
+		return "", err
+	}
+
+	for {
+		var err error
+		r, err = incString(r)
+		if err != nil {
+			return "", err
+		}
+
+		if len(r) > 8 {
+			err := fmt.Errorf("no result found for %q", s)
+			return "", err
+		}
+
+		if hasNoBad(r) && hasStraight(r) && hasRepeated(r) {
+			break
+		}
+	}
+
+	result := string(r)
+
+	return result, nil
+}
+
+func process(s string) (string, error) {
+	result, err := nextPassword(s)
+	if err != nil {
+		return "", err
+	}
+
+	return result, nil
+}
+
+func run() int {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "%s filename\n", os.Args[0])
+		return 1
+	}
+
+	b, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+	s := strings.TrimSpace(string(b))
+
+	v, err := process(s)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+
+	fmt.Printf("result: %q\n", v)
+
+	return 0
+}
+
+func main() {
+	os.Exit(run())
+}
---
+++ day11/day11_test.go
@@ -0,0 +1,217 @@
+package main
+
+import "testing"
+
+func TestIncRune(t *testing.T) {
+	tests := []struct {
+		in   rune
+		out  rune
+		outC bool
+	}{
+		{'a', 'b', false},
+		{'m', 'n', false},
+		{'z', 'a', true},
+	}
+
+	for _, tt := range tests {
+		v, c, err := incRune(tt.in)
+		if err != nil {
+			t.Errorf("incRune(%q) = error %s, want %q, %t", tt.in, err, tt.out, tt.outC)
+		} else if v != tt.out || c != tt.outC {
+			t.Errorf("incRune(%q) = %q, %t, want %q, %t", tt.in, v, c, tt.out, tt.outC)
+		}
+	}
+}
+
+func TestIncRuneError(t *testing.T) {
+	tests := []rune{
+		0,
+		'A',
+		'☃',
+	}
+
+	for _, tt := range tests {
+		v, c, err := incRune(tt)
+		if err == nil {
+			t.Errorf("incRune(%q) = %q, %t, want error", tt, v, c)
+		}
+	}
+}
+
+func TestIncString(t *testing.T) {
+	tests := []struct {
+		in  string
+		out string
+	}{
+		{"a", "b"},
+		{"m", "n"},
+		{"z", "aa"},
+		{"xyzzzzzz", "xzaaaaaa"},
+	}
+
+	for _, tt := range tests {
+		r, err := incString([]rune(tt.in))
+		v := string(r)
+		if err != nil {
+			t.Errorf("incString(%q) = error %s, want %q", tt.in, err, tt.out)
+		} else if v != tt.out {
+			t.Errorf("incString(%q) = %q, want %q", tt.in, v, tt.out)
+		}
+	}
+}
+
+func TestIncStringError(t *testing.T) {
+	tests := []string{
+		"\x00",
+		"A",
+		"☃",
+		"abc\x00",
+	}
+
+	for _, tt := range tests {
+		r, err := incString([]rune(tt))
+		v := string(r)
+		if err == nil {
+			t.Errorf("incString(%q) = %q, want error", tt, v)
+		}
+	}
+}
+
+func TestHasNoBad(t *testing.T) {
+	tests := []struct {
+		in  string
+		out bool
+	}{
+		{"a", true},
+		{"m", true},
+		{"xyzzzzzz", true},
+		{"o", false},
+		{"i", false},
+		{"l", false},
+		{"oiler", false},
+	}
+
+	for _, tt := range tests {
+		v := hasNoBad([]rune(tt.in))
+		if v != tt.out {
+			t.Errorf("hasNoBad(%q) = %t, want %t", tt.in, v, tt.out)
+		}
+	}
+}
+
+func TestHasStraight(t *testing.T) {
+	tests := []struct {
+		in  string
+		out bool
+	}{
+		{"a", false},
+		{"ab", false},
+		{"abc", true},
+		{"abbc", false},
+		{"hijklmmn", true},
+		{"abbcemno", true},
+	}
+
+	for _, tt := range tests {
+		v := hasStraight([]rune(tt.in))
+		if v != tt.out {
+			t.Errorf("hasStraight(%q) = %t, want %t", tt.in, v, tt.out)
+		}
+	}
+}
+
+func TestHasRepeated(t *testing.T) {
+	tests := []struct {
+		in  string
+		out bool
+	}{
+		{"a", false},
+		{"aa", false},
+		{"aaa", false},
+		{"aabaa", false},
+		{"aabb", true},
+		{"abbcegjk", false},
+		{"abbceffg", true},
+	}
+
+	for _, tt := range tests {
+		v := hasRepeated([]rune(tt.in))
+		if v != tt.out {
+			t.Errorf("hasRepeated(%q) = %t, want %t", tt.in, v, tt.out)
+		}
+	}
+}
+
+func TestNextPassword(t *testing.T) {
+	tests := []struct {
+		in  string
+		out string
+	}{
+		{"abcdefgh", "abcdffaa"},
+		{"ghijklmn", "ghjaabcc"},
+	}
+
+	for _, tt := range tests {
+		v, err := nextPassword(tt.in)
+		if err != nil {
+			t.Errorf("nextPassword(%q) = error %s, want %q", tt.in, err, tt.out)
+		} else if v != tt.out {
+			t.Errorf("nextPassword(%q) = %q, want %q", tt.in, v, tt.out)
+		}
+	}
+}
+
+func TestNextPasswordError(t *testing.T) {
+	tests := []string{
+		"\x00",
+		"A",
+		"☃",
+		"abc\x00",
+		"aaaaaaaaa",
+		"zzzzzzzz",
+	}
+
+	for _, tt := range tests {
+		v, err := nextPassword(tt)
+		if err == nil {
+			t.Errorf("nextPassword(%q) = %q, want error", tt, v)
+		}
+	}
+}
+
+func TestProcess(t *testing.T) {
+	tests := []struct {
+		in  string
+		out string
+	}{
+		{"abcdefgh", "abcdffaa"},
+		{"ghijklmn", "ghjaabcc"},
+	}
+
+	for _, tt := range tests {
+		v, err := process(tt.in)
+		if err != nil {
+			t.Errorf("process(%q) = error %s, want %q", tt.in, err, tt.out)
+		} else if v != tt.out {
+			t.Errorf("process(%q) = %q, want %q", tt.in, v, tt.out)
+		}
+	}
+}
+
+func TestProcessError(t *testing.T) {
+	tests := []string{
+		"\x00",
+		"A",
+		"☃",
+		"abc\x00",
+		"aaaaaaaaa",
+		"zzzzzzzz",
+	}
+
+	for _, tt := range tests {
+		v, err := process(tt)
+		if err == nil {
+			t.Errorf("process(%q) = %q, want error", tt, v)
+		}
+	}
+}
