335217e596676ce9c53196dfadcbefd6e05eac9e
Andrew McRae <dev@fesh.geek.nz>
2016-01-22 15:03:15 +1300

    day14 part 1 complete
    passes tests and correct result

---
+++ day14/day14.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+var lineRegexp = regexp.MustCompile(
+	`^(.+) can fly ([0-9]+) km/s for ([0-9]+) seconds, but then must rest for ([0-9]+) seconds\.$`)
+
+func parseLine(s string) (string, int, int, int, error) {
+	errFormat := fmt.Errorf("invalid line %q", s)
+
+	m := lineRegexp.FindStringSubmatch(s)
+	if m == nil {
+		return "", 0, 0, 0, errFormat
+	}
+
+	name := m[1]
+
+	speed, err := strconv.Atoi(m[2])
+	if err != nil {
+		return "", 0, 0, 0, errFormat
+	}
+
+	fly, err := strconv.Atoi(m[3])
+	if err != nil {
+		return "", 0, 0, 0, errFormat
+	}
+
+	rest, err := strconv.Atoi(m[4])
+	if err != nil {
+		return "", 0, 0, 0, errFormat
+	}
+
+	return name, speed, fly, rest, nil
+}
+
+type deer struct {
+	name     string
+	speed    int
+	fly      int
+	rest     int
+	distance int
+	flying   bool
+	next     int
+}
+
+func process(f io.Reader, endTime int) (int, error) {
+	var allDeer []deer
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		s := scanner.Text()
+
+		name, speed, fly, rest, err := parseLine(s)
+		if err != nil {
+			return 0, err
+		}
+
+		allDeer = append(allDeer, deer{name: name, speed: speed, fly: fly, rest: rest})
+	}
+
+	for t := 0; t < endTime; t++ {
+		for k := range allDeer {
+			d := &allDeer[k]
+
+			if d.next == t {
+				if d.flying {
+					d.flying = false
+					d.next += d.rest
+				} else {
+					d.flying = true
+					d.next += d.fly
+				}
+			}
+
+			if d.flying {
+				d.distance += d.speed
+			}
+		}
+	}
+
+	var maxDistance int
+	for i, d := range allDeer {
+		if i == 0 || d.distance > maxDistance {
+			maxDistance = d.distance
+		}
+	}
+
+	return maxDistance, nil
+}
+
+func run() int {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "%s filename\n", os.Args[0])
+		return 1
+	}
+
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+	defer f.Close()
+
+	distance, err := process(f, 2503)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+
+	fmt.Printf("distance: %d\n", distance)
+
+	return 0
+}
+
+func main() {
+	os.Exit(run())
+}
---
+++ day14/day14_test.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestParseLine(t *testing.T) {
+	tests := []struct {
+		in       string
+		outName  string
+		outSpeed int
+		outFly   int
+		outRest  int
+	}{
+		{"Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.", "Comet", 14, 10, 127},
+		{"Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.", "Dancer", 16, 11, 162},
+	}
+
+	for _, tt := range tests {
+		name, speed, fly, rest, err := parseLine(tt.in)
+		if err != nil {
+			t.Errorf("parseLine(%q) = error %s, want %s, %d, %d, %d",
+				tt.in, err, tt.outName, tt.outSpeed, tt.outFly, tt.outRest)
+		} else if name != tt.outName || speed != tt.outSpeed || fly != tt.outFly || rest != tt.outRest {
+			t.Errorf("parseLine(%q) = %s, %d, %d, %d, want %s, %d, %d, %d",
+				tt.in, name, speed, fly, rest, tt.outName, tt.outSpeed, tt.outFly, tt.outRest)
+		}
+	}
+}
+
+func TestParseLineError(t *testing.T) {
+	tests := []string{
+		"",
+		"Comet",
+		"Comet can fly 14Z km/s for 10 seconds, but then must rest for 127 seconds.",
+		"Comet can fly 14 km/s for 10.0 seconds, but then must rest for 127 seconds.",
+		"Comet can fly 14 km/s for 10.0 seconds, but then must rest for ABC seconds.",
+	}
+
+	for _, tt := range tests {
+		name, speed, fly, rest, err := parseLine(tt)
+		if err == nil {
+			t.Errorf("parseLine(%q) = %s, %d, %d, %d, want error", tt, name, speed, fly, rest)
+		}
+	}
+}
+
+func TestProcess(t *testing.T) {
+	tests := []struct {
+		in          string
+		inTime      int
+		outDistance int
+	}{
+		{"Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.\nDancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.\n",
+			1000, 1120},
+	}
+
+	for _, tt := range tests {
+		distance, err := process(strings.NewReader(tt.in), tt.inTime)
+		if err != nil {
+			t.Errorf("process(%q, %d) = error %s, want %d", tt.in, tt.inTime, err, tt.outDistance)
+		} else if distance != tt.outDistance {
+			t.Errorf("process(%q, %d) = %d, want %d", tt.in, tt.inTime, distance, tt.outDistance)
+		}
+	}
+}
+
+func TestProcessError(t *testing.T) {
+	tests := []struct {
+		in     string
+		inTime int
+	}{
+		{"Comet", 1000},
+		{"Comet can fly 14Z km/s for 10 seconds, but then must rest for 127 seconds.", 1000},
+		{"Comet can fly 14 km/s for 10.0 seconds, but then must rest for 127 seconds.", 1000},
+		{"Comet can fly 14 km/s for 10.0 seconds, but then must rest for ABC seconds.", 1000},
+	}
+
+	for _, tt := range tests {
+		distance, err := process(strings.NewReader(tt.in), tt.inTime)
+		if err == nil {
+			t.Errorf("process(%q, %d) = %d, want error", tt.in, tt.inTime, distance)
+		}
+	}
+}
