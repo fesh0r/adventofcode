cb3219673cc8ec59845a1a50ac71e21f260e1610
Andrew McRae <dev@fesh.geek.nz>
2016-01-22 15:53:29 +1300

    day15 part 1 complete
    tests pass and solution correct

---
+++ day15/day15.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+var lineRegexp = regexp.MustCompile(
+	`^(.+): capacity ([0-9-]+), durability ([0-9-]+), flavor ([0-9-]+), texture ([0-9-]+), calories ([0-9-]+)$`)
+
+func parseLine(s string) (string, int, int, int, int, int, error) {
+	errFormat := fmt.Errorf("invalid line %q", s)
+
+	m := lineRegexp.FindStringSubmatch(s)
+	if m == nil {
+		return "", 0, 0, 0, 0, 0, errFormat
+	}
+
+	name := m[1]
+
+	capacity, err := strconv.Atoi(m[2])
+	if err != nil {
+		return "", 0, 0, 0, 0, 0, errFormat
+	}
+
+	durability, err := strconv.Atoi(m[3])
+	if err != nil {
+		return "", 0, 0, 0, 0, 0, errFormat
+	}
+
+	flavor, err := strconv.Atoi(m[4])
+	if err != nil {
+		return "", 0, 0, 0, 0, 0, errFormat
+	}
+
+	texture, err := strconv.Atoi(m[5])
+	if err != nil {
+		return "", 0, 0, 0, 0, 0, errFormat
+	}
+
+	calories, err := strconv.Atoi(m[6])
+	if err != nil {
+		return "", 0, 0, 0, 0, 0, errFormat
+	}
+
+	return name, capacity, durability, flavor, texture, calories, nil
+}
+
+func combinations(n int, r int) <-chan []int {
+	c := make(chan []int)
+	go func() {
+		defer close(c)
+
+		indices := make([]int, r)
+		for i := range indices {
+			indices[i] = 0
+		}
+
+		out := make([]int, len(indices))
+		copy(out, indices)
+		c <- out
+
+		for n > 0 {
+			i := r - 1
+			for ; i >= 0; i-- {
+				if indices[i] != n-1 {
+					break
+				}
+			}
+
+			if i < 0 {
+				break
+			}
+
+			index := indices[i]
+			for j := i; j < r; j++ {
+				indices[j] = index + 1
+			}
+
+			out := make([]int, len(indices))
+			copy(out, indices)
+			c <- out
+		}
+	}()
+
+	return c
+}
+
+type ingredient struct {
+	name       string
+	capacity   int
+	durability int
+	flavour    int
+	texture    int
+	calories   int
+}
+
+func process(f io.Reader, n int) (int, error) {
+	var ingredients []ingredient
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		s := scanner.Text()
+
+		name, capacity, durability, flavor, texture, calories, err := parseLine(s)
+		if err != nil {
+			return 0, err
+		}
+
+		ingredients = append(ingredients, ingredient{name, capacity, durability, flavor, texture, calories})
+	}
+
+	var highest int
+	for c := range combinations(len(ingredients), n) {
+		var t ingredient
+		var cur int
+		for _, i := range c {
+			t.capacity += ingredients[i].capacity
+			t.durability += ingredients[i].durability
+			t.flavour += ingredients[i].flavour
+			t.texture += ingredients[i].texture
+		}
+		if t.capacity < 0 {
+			t.capacity = 0
+		}
+		if t.durability < 0 {
+			t.durability = 0
+		}
+		if t.flavour < 0 {
+			t.flavour = 0
+		}
+		if t.texture < 0 {
+			t.texture = 0
+		}
+		cur = t.capacity * t.durability * t.flavour * t.texture
+		if cur > highest {
+			highest = cur
+		}
+	}
+
+	return highest, nil
+}
+
+func run() int {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "%s filename\n", os.Args[0])
+		return 1
+	}
+
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+	defer f.Close()
+
+	score, err := process(f, 100)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+
+	fmt.Printf("score: %d\n", score)
+
+	return 0
+}
+
+func main() {
+	os.Exit(run())
+}
---
+++ day15/day15_test.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestParseLine(t *testing.T) {
+	tests := []struct {
+		in            string
+		outName       string
+		outCapacity   int
+		outDurability int
+		outFlavor     int
+		outTexture    int
+		outCalories   int
+	}{
+		{"Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8", "Butterscotch", -1, -2, 6, 3, 8},
+		{"Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3", "Cinnamon", 2, 3, -2, -1, 3},
+	}
+
+	for _, tt := range tests {
+		name, capacity, durability, flavor, texture, calories, err := parseLine(tt.in)
+		if err != nil {
+			t.Errorf("parseLine(%q) = error %s, want %s, %d, %d, %d, %d, %d",
+				tt.in, err, tt.outName, tt.outCapacity, tt.outDurability, tt.outFlavor, tt.outTexture, tt.outCalories)
+		} else if name != tt.outName || capacity != tt.outCapacity || durability != tt.outDurability ||
+			flavor != tt.outFlavor || texture != tt.outTexture || calories != tt.outCalories {
+			t.Errorf("parseLine(%q) = %s, %d, %d, %d, %d, %d, want %s, %d, %d, %d, %d, %d",
+				tt.in, name, capacity, durability, flavor, texture, calories, tt.outName, tt.outCapacity,
+				tt.outDurability, tt.outFlavor, tt.outTexture, tt.outCalories)
+		}
+	}
+}
+
+func TestParseLineError(t *testing.T) {
+	tests := []string{
+		"",
+		"Butterscotch",
+		"Butterscotch: capacity -1.0, durability -2, flavor 6, texture 3, calories 8",
+		"Butterscotch: capacity -1A, durability -2, flavor 6, texture 3, calories 8",
+		"Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories AZ",
+		"Butterscotch: capacity 0-1, durability -2, flavor 6, texture 3, calories 8",
+	}
+
+	for _, tt := range tests {
+		name, capacity, durability, flavor, texture, calories, err := parseLine(tt)
+		if err == nil {
+			t.Errorf("parseLine(%q) = %s, %d, %d, %d, %d, %d, want error",
+				tt, name, capacity, durability, flavor, texture, calories)
+		}
+	}
+}
+
+func TestCombinations(t *testing.T) {
+	tests := []struct {
+		inN int
+		inR int
+		out [][]int
+	}{
+		{1, 1, [][]int{
+			{0},
+		}},
+		{2, 1, [][]int{
+			{0}, {1},
+		}},
+		{2, 2, [][]int{
+			{0, 0}, {0, 1}, {1, 1},
+		}},
+		{4, 2, [][]int{
+			{0, 0}, {0, 1}, {0, 2}, {0, 3}, {1, 1}, {1, 2}, {1, 3}, {2, 2}, {2, 3}, {3, 3},
+		}},
+	}
+
+	for _, tt := range tests {
+		var out [][]int
+		for v := range combinations(tt.inN, tt.inR) {
+			out = append(out, v)
+		}
+		if !reflect.DeepEqual(out, tt.out) {
+			t.Errorf("permutations(%d, %d) = %v, want %v", tt.inN, tt.inR, out, tt.out)
+		}
+	}
+}
+
+func TestProcess(t *testing.T) {
+	tests := []struct {
+		in       string
+		inN      int
+		outScore int
+	}{
+		{"Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8\nCinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3",
+			100, 62842880},
+	}
+
+	for _, tt := range tests {
+		score, err := process(strings.NewReader(tt.in), tt.inN)
+		if err != nil {
+			t.Errorf("process(%q, %d) = error %s, want %d", tt.in, tt.inN, err, tt.outScore)
+		} else if score != tt.outScore {
+			t.Errorf("process(%q, %d) = %d want %d", tt.in, tt.inN, score, tt.outScore)
+		}
+	}
+}
+
+func TestProcessError(t *testing.T) {
+	tests := []struct {
+		in  string
+		inN int
+	}{
+		{"Butterscotch", 100},
+		{"Butterscotch: capacity -1.0, durability -2, flavor 6, texture 3, calories 8", 100},
+		{"Butterscotch: capacity -1A, durability -2, flavor 6, texture 3, calories 8", 100},
+		{"Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories AZ", 100},
+		{"Butterscotch: capacity 0-1, durability -2, flavor 6, texture 3, calories 8", 100},
+	}
+
+	for _, tt := range tests {
+		score, err := process(strings.NewReader(tt.in), tt.inN)
+		if err == nil {
+			t.Errorf("process(%q, %d) = %d, want error", tt.in, tt.inN, score)
+		}
+	}
+}
