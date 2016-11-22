614a42e5a82b4f9271802b5e4725352a5731e361
Andrew McRae <andrew@staff.inspire.net.nz>
2016-11-22 17:04:25 +1300

    day 18 part 1 done, and tests

---
+++ day18/day18.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+type Board struct {
+	b    [][]bool
+	w, h int
+}
+
+func NewBoard(w, h int) *Board {
+	b := make([][]bool, h+2)
+
+	for i := range b {
+		b[i] = make([]bool, w+2)
+	}
+
+	return &Board{b, w, h}
+}
+
+func (b *Board) String() string {
+	var s string
+
+	for y := 0; y < b.h; y++ {
+		for x := 0; x < b.w; x++ {
+			if b.On(x, y) {
+				s += "#"
+			} else {
+				s += "."
+			}
+		}
+		if y < b.h-1 {
+			s += "\n"
+		}
+	}
+
+	return s
+}
+
+func (b *Board) On(x, y int) bool {
+	return b.b[y+1][x+1]
+}
+
+func (b *Board) Set(x, y int, alive bool) {
+	b.b[y+1][x+1] = alive
+}
+
+func (b *Board) Next(x, y int) bool {
+	c := 0
+	for ny := -1; ny <= 1; ny++ {
+		for nx := -1; nx <= 1; nx++ {
+			if (nx != 0 || ny != 0) && b.On(x+nx, y+ny) {
+				c++
+			}
+		}
+	}
+
+	return c == 3 || c == 2 && b.On(x, y)
+}
+
+type Life struct {
+	a, b *Board
+	w, h int
+}
+
+func NewLife(s string) *Life {
+	ss := strings.Split(s, "\n")
+
+	w := len(ss[0])
+	h := len(ss)
+	a := NewBoard(w, h)
+
+	for y, row := range ss {
+		for x, cell := range row {
+			a.Set(x, y, cell == '#')
+		}
+	}
+
+	return &Life{a, NewBoard(w, h), w, h}
+}
+
+func (l *Life) String() string {
+	return l.a.String()
+}
+
+func (l *Life) Next() {
+	for y := 0; y < l.h; y++ {
+		for x := 0; x < l.w; x++ {
+			l.b.Set(x, y, l.a.Next(x, y))
+		}
+	}
+
+	l.a, l.b = l.b, l.a
+}
+
+func (l *Life) On() int {
+	on := 0
+	for y := 0; y < l.h; y++ {
+		for x := 0; x < l.w; x++ {
+			if l.a.On(x, y) {
+				on++
+			}
+		}
+	}
+	return on
+}
+
+func process(s string, g int) (int, error) {
+	l := NewLife(s)
+
+	for i := 0; i < g; i++ {
+		l.Next()
+	}
+
+	on := l.On()
+
+	return on, nil
+}
+
+func run() int {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "%s filename\n", os.Args[0])
+		return 1
+	}
+
+	b, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+	s := strings.TrimSpace(string(b))
+
+	l, err := process(s, 100)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+
+	fmt.Printf("lights: %d\n", l)
+
+	return 0
+}
+
+func main() {
+	os.Exit(run())
+}
---
+++ day18/day18_test.go
@@ -0,0 +1,278 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func getGliderBoard() *Board {
+	return &Board{
+		[][]bool{
+			{false, false, false, false, false, false, false},
+			{false, false, false, false, false, false, false},
+			{false, false, false, true, false, false, false},
+			{false, false, false, false, true, false, false},
+			{false, false, true, true, true, false, false},
+			{false, false, false, false, false, false, false},
+			{false, false, false, false, false, false, false},
+		}, 5, 5}
+}
+
+func getGliderLife() *Life {
+	return &Life{getGliderBoard(), NewBoard(5, 5), 5, 5}
+}
+
+func getGliderString() string {
+	return ".....\n..#..\n...#.\n.###.\n....."
+}
+
+func TestNewBoard(t *testing.T) {
+	tests := []struct {
+		inX, inY int
+		out      Board
+	}{
+		{2, 2, Board{
+			[][]bool{
+				{false, false, false, false},
+				{false, false, false, false},
+				{false, false, false, false},
+				{false, false, false, false},
+			}, 2, 2},
+		},
+		{1, 4, Board{
+			[][]bool{
+				{false, false, false},
+				{false, false, false},
+				{false, false, false},
+				{false, false, false},
+				{false, false, false},
+				{false, false, false},
+			}, 1, 4},
+		},
+	}
+
+	for _, tt := range tests {
+		board := NewBoard(tt.inX, tt.inY)
+		if !reflect.DeepEqual(board, &tt.out) {
+			t.Errorf("NewBoard(%d,%d) = %#v, want %#v", tt.inX, tt.inY, board, tt.out)
+		}
+	}
+}
+
+func TestBoard_String(t *testing.T) {
+	tests := []struct {
+		in  Board
+		out string
+	}{
+		{
+			Board{
+				[][]bool{
+					{false, false, false},
+					{false, true, false},
+					{false, false, false},
+				}, 1, 1},
+			"#",
+		},
+		{
+			*getGliderBoard(),
+			".....\n..#..\n...#.\n.###.\n.....",
+		},
+	}
+
+	for _, tt := range tests {
+		out := tt.in.String()
+		if out != tt.out {
+			t.Errorf("Board.String() = %q, want %q", out, tt.out)
+		}
+	}
+}
+
+func TestBoard_On(t *testing.T) {
+	tests := []struct {
+		inX, inY int
+		out      bool
+	}{
+		{1, 1, false},
+		{3, 2, true},
+		{3, 3, true},
+	}
+
+	for _, tt := range tests {
+		b := getGliderBoard()
+		out := b.On(tt.inX, tt.inY)
+		if out != tt.out {
+			t.Errorf("Board.On(%d,%d) = %v, want %v", tt.inX, tt.inY, out, tt.out)
+		}
+	}
+}
+
+func TestBoard_Set(t *testing.T) {
+	tests := []struct {
+		inX, inY int
+		inOn     bool
+		out      bool
+	}{
+		{0, 0, false, false},
+		{2, 1, false, false},
+		{2, 2, true, true},
+		{3, 2, true, true},
+	}
+
+	for _, tt := range tests {
+		b := getGliderBoard()
+		b.Set(tt.inX, tt.inY, tt.inOn)
+		out := b.On(tt.inX, tt.inY)
+		if out != tt.out {
+			t.Errorf("Board.Set(%d,%d,%v) = %v, want %v", tt.inX, tt.inY, tt.inOn, out, tt.out)
+		}
+	}
+}
+
+func TestBoard_Next(t *testing.T) {
+	tests := []struct {
+		inX, inY int
+		out      bool
+	}{
+		{0, 0, false},
+		{2, 1, false},
+		{2, 2, false},
+		{3, 2, true},
+	}
+
+	for _, tt := range tests {
+		b := getGliderBoard()
+		out := b.Next(tt.inX, tt.inY)
+		if out != tt.out {
+			t.Errorf("Board.Next(%d,%d) = %v, want %v", tt.inX, tt.inY, out, tt.out)
+		}
+	}
+}
+
+func TestNewLife(t *testing.T) {
+	tests := []struct {
+		in  string
+		out Life
+	}{
+		{
+			".....\n..#..\n...#.\n.###.\n.....",
+			*getGliderLife(),
+		},
+	}
+
+	for _, tt := range tests {
+		life := NewLife(tt.in)
+		if !reflect.DeepEqual(life, &tt.out) {
+			t.Errorf("NewLife(%q) = %#v, want %#v", tt.in, life.a, tt.out.a)
+		}
+	}
+}
+
+func TestLife_String(t *testing.T) {
+	tests := []struct {
+		in  Life
+		out string
+	}{
+		{
+			*getGliderLife(),
+			getGliderString(),
+		},
+	}
+
+	for _, tt := range tests {
+		out := tt.in.String()
+		if out != tt.out {
+			t.Errorf("Life.String() = %q, want %q", out, tt.out)
+		}
+	}
+}
+
+func TestLife_Next(t *testing.T) {
+	tests := []struct {
+		in  Life
+		out string
+	}{
+		{
+			*getGliderLife(),
+			".....\n.....\n.#.#.\n..##.\n..#..",
+		},
+		{
+			*NewLife("...\n###\n..."),
+			".#.\n.#.\n.#.",
+		},
+		{
+			*NewLife(".#.#.#\n...##.\n#....#\n..#...\n#.#..#\n####.."),
+			"..##..\n..##.#\n...##.\n......\n#.....\n#.##..",
+		},
+		{
+			*NewLife("..##..\n..##.#\n...##.\n......\n#.....\n#.##.."),
+			"..###.\n......\n..###.\n......\n.#....\n.#....",
+		},
+		{
+			*NewLife("..###.\n......\n..###.\n......\n.#....\n.#...."),
+			"...#..\n......\n...#..\n..##..\n......\n......",
+		},
+		{
+			*NewLife("...#..\n......\n...#..\n..##..\n......\n......"),
+			"......\n......\n..##..\n..##..\n......\n......",
+		},
+	}
+
+	for _, tt := range tests {
+		tt.in.Next()
+		out := tt.in.String()
+		if out != tt.out {
+			t.Errorf("Life.Next() = %q, want %q", out, tt.out)
+		}
+	}
+}
+
+func TestLife_On(t *testing.T) {
+	tests := []struct {
+		in  Life
+		out int
+	}{
+		{
+			*getGliderLife(),
+			5,
+		},
+		{
+			*NewLife(".#.#.#\n...##.\n#....#\n..#...\n#.#..#\n####.."),
+			15,
+		},
+		{
+			*NewLife("......\n......\n..##..\n..##..\n......\n......"),
+			4,
+		},
+	}
+
+	for _, tt := range tests {
+		out := tt.in.On()
+		if out != tt.out {
+			t.Errorf("Life.On() = %d, want %d", out, tt.out)
+		}
+	}
+}
+
+func TestProcess(t *testing.T) {
+	tests := []struct {
+		in    string
+		inGen int
+		out   int
+	}{
+		{
+			getGliderString(), 4,
+			5,
+		},
+		{
+			".#.#.#\n...##.\n#....#\n..#...\n#.#..#\n####..", 4,
+			4,
+		},
+	}
+
+	for _, tt := range tests {
+		out, _ := process(tt.in, tt.inGen)
+		if out != tt.out {
+			t.Errorf("process() = %d, want %d", out, tt.out)
+		}
+	}
+}
