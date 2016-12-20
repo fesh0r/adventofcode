21671db6716ab23d6b76bc8705bb4e24ee5da9f1
Andrew McRae <andrew@staff.inspire.net.nz>
2016-12-20 16:26:35 +1300

    day 20 2015 part 2

--- 2015/day20/day20.go
+++ 2015/day20/day20.go
@@ -34,6 +34,32 @@ func process(s string) (int, error) {
 	return res, nil
 }
 
+func process2(s string) (int, error) {
+	p, err := strconv.Atoi(s)
+	if err != nil {
+		return 0, err
+	}
+
+	v := make([]int, p / 11)
+
+	for i := 1; i < len(v); i++ {
+		for j, k := i, 0; j < len(v) && k < 50; j, k = j+i, k+1 {
+			v[j] += i * 11
+		}
+	}
+
+	res := -1
+
+	for i := 1; i < len(v); i++ {
+		if v[i] >= p {
+			res = i
+			break
+		}
+	}
+
+	return res, nil
+}
+
 func run() int {
 	if len(os.Args) != 2 {
 		fmt.Fprintf(os.Stderr, "%s filename\n", os.Args[0])
@@ -53,7 +79,13 @@ func run() int {
 		return 1
 	}
 
-	fmt.Printf("house: %d\n", house)
+	house2, err := process2(s)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+
+	fmt.Printf("house: %d\nhouse2: %d\n", house, house2)
 	return 0
 }
 
--- 2015/day20/day20_test.go
+++ 2015/day20/day20_test.go
@@ -22,3 +22,22 @@ func TestProcess(t *testing.T) {
 		}
 	}
 }
+
+func TestProcess2(t *testing.T) {
+	tests := []struct {
+		in  string
+		out int
+	}{
+		{"150", 8},
+		{"1000", 36},
+	}
+
+	for _, tt := range tests {
+		c, err := process2(tt.in)
+		if err != nil {
+			t.Errorf("process2(%q) = error %s, want %d", tt.in, err, tt.out)
+		} else if c != tt.out {
+			t.Errorf("process2(%q) = %d, want %d", tt.in, c, tt.out)
+		}
+	}
+}
